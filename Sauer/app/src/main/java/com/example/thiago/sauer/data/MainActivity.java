e093d17f91354cbec26ec856d0dd4b93f9312e21
AlvaroSK <alvaro.pepper@gmail.com>
2020-06-17 14:57:57 -0300

    Front

---
+++ Sauer/app/src/main/java/com/example/thiago/sauer/data/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.thiago.sauer.data;
+
+import android.widget.ArrayAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
---
+++ Sauer/app/src/main/java/com/example/thiago/sauer/menu_paciente.java
@@ -0,0 +1,14 @@
+package com.example.thiago.sauer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_paciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_paciente);
+    }
+}
